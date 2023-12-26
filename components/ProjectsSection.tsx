ef65fb929b831cb46b15dc9794616a2eabd297e4
Kofi <112496906+kamoah233@users.noreply.github.com>
2023-12-26 12:29:53 -0500

    removed out of place projectSection text

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -35,7 +35,7 @@ const projects = [
 
 const ProjectsSection = () => {
     return (
-        <section id="projects">ProjectsSection
+        <section id="projects">
         <h1 className='text-center font-bold text-4xl'>
             Projects
             <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
